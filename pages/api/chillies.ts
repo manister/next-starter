960d831e2c5b3b60b68cab763aa4ac67ec927c0c
Manister <harry@toolshed.design>
2022-02-23 20:57:41 +0000

    correct path

--- pages/api/chillies.ts
+++ pages/api/chillies.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { IChilliData } from '~/lib/types'
 import { importAll } from '~/lib/webpack-helpers'
 
-const chilliData = importAll(require.context('../../../_content/chillies', false, /\.md$/)) as IChilliData
+const chilliData = importAll(require.context('../../_content/chillies', false, /\.md$/)) as IChilliData
 
 const handler = (_req: NextApiRequest, res: NextApiResponse): void => {
   res.status(200).json(chilliData)
