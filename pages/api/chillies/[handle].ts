0d0e9c358e88ff1f84ed9ff6705927c42875cb55
Manister <harry@toolshed.design>
2022-02-23 21:41:07 +0000

    404

--- pages/api/chillies/[handle].ts
+++ pages/api/chillies/[handle].ts
@@ -9,7 +9,7 @@ const handler = (req: NextApiRequest, res: NextApiResponse): void => {
   if (handle && typeof handle === 'string' && chilliData[handle]) {
     res.status(200).json(chilliData[handle])
   } else {
-    res.status(404)
+    res.status(404).send('')
   }
 }
 
